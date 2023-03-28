0cc7f4b792b032f686a7e2bce305b02aca2cb6fc
abiola <abiola.arobieke@gmail.com>
2023-03-28 15:31:45 -0400

    Add function to remove book for books array

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-const bookList = document.getElementById("book-list");
-const addBookForm = document.getElementById("add-book-form");
-const titleInput = document.getElementById("title-input");
-const authorInput = document.getElementById("author-input");
+const bookList = document.getElementById('book-list');
+const addBookForm = document.getElementById('add-book-form');
+const titleInput = document.getElementById('title-input');
+const authorInput = document.getElementById('author-input');
 
 let books = [];
 
 function updateBookList() {
-  bookList.innerHTML = "";
+  bookList.innerHTML = '';
   for (let i = 0; i < books.length; i++) {
     const book = books[i];
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
     listItem.innerHTML = `<strong>${book.title}</strong> <br> ${book.author}<br>`;
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", () => {
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => {
       removeBook(book.title);
     });
-    const breakline = document.createElement("hr");
+    const breakline = document.createElement('hr');
     listItem.appendChild(removeButton);
     listItem.appendChild(breakline);
     bookList.appendChild(listItem);
   }
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-addBookForm.addEventListener("submit", (event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const title = titleInput.value;
   const author = authorInput.value;
   const book = { title, author };
   books.push(book);
-  titleInput.value = "";
-  authorInput.value = "";
+  titleInput.value = '';
+  authorInput.value = '';
   updateBookList();
+});
+
+function removeBook(title) {
+  books = books.filter((book) => book.title !== title);
+  updateBookList();
+}
+
+window.addEventListener('load', () => {
+  const storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+    updateBookList();
+  }
 });
